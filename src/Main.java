e875a8a8f990196def49ec3d8c5b2344d41060f4
taircohen94 <tairc@post.bgu.ac.il>
2020-05-29 12:00:19 +0300

    connecting to server

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 import AssociationAssets.*;
+import DAL.JDBCConnector;
 import Model.Model;
 import Server.Server;
 import Server.Strategies.ServerSender;
@@ -15,65 +16,66 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
         Model myModel = new Model();
     }
 
     private static void initDB() {
-        Season season = new Season("2020");
-        Season season1 = new Season("2021");
-        FootballSystem.getInstance().addSeasonToDB(season);
-        FootballSystem.getInstance().addSeasonToDB(season1);
-        League league = new League("La Liga");
-        League league1 = new League("liga");
-        FootballSystem.getInstance().addLeagueToDB(league);
-        FootballSystem.getInstance().addLeagueToDB(league1);
-        league.addSeasonToLeague(season);
-        Field field = new Field("Blomfield","teal aviv",1000);
-        Field field1 = new Field("Tedi","teal aviv",1000);
-        FootballSystem.getInstance().addFieldToDB(field);
-        FootballSystem.getInstance().addFieldToDB(field1);
-        FootballSystem.getInstance().signIn("4","4","tair","cohen");
-        TeamOwner tairTO = (TeamOwner) FootballSystem.getInstance().creatingTeamOwner("4","tair","cohen");
-        FootballSystem.getInstance().signIn("1","1","lala","la");
-        FootballSystem.getInstance().creatingReferee("1","la","laala", EReferee.MAIN);
-        FootballSystem.getInstance().signIn("2","2","lala","la");
-        FootballSystem.getInstance().creatingReferee("2","la","laala", EReferee.ASSISTANT);
-        FootballSystem.getInstance().signIn("3","3","lala","la");
-        FootballSystem.getInstance().creatingReferee("3","la","laala", EReferee.ASSISTANT);
-
-        Team team1 = new Team(45,"team1",season,field,null,(TeamOwner)FootballSystem.getInstance().getFanByUserName("4"));
-        Team team2 = new Team(55,"team2",season,field,null,(TeamOwner)FootballSystem.getInstance().getFanByUserName("4"));
-        team1.addSeasonToTeam(season);
-        team1.addSeasonToTeam(season1);
-        team2.addSeasonToTeam(season);
-        tairTO.addCoach(team1,season,"coach-2020","123","c","c", ETraining.CDiploma,ECoachRole.AssistantCoach);
-        tairTO.addField(team1,season,"Camp-No1","tel-aviv",12222);
-        tairTO.addField(team1,season,"Camp-No2","tel-aviv",12222);
-        tairTO.addTeamManager(team1,season,"TM-2020","123","la","la");
-        tairTO.addPlayer(team1,season,"player-2020","123","la","la",new Date(17/10/1995),EPlayerRole.GoalKeeper);
-
-        try {
-            FootballSystem.getInstance().addTeamToDB(team1);
-            FootballSystem.getInstance().addTeamToDB(team2);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Date date = new Date(2020-1900,4,12);
-        Time time = new Time(14,40,0);
-        Referee main = FootballSystem.getInstance().getRefereeByUseName("1");
-        Referee side1 = FootballSystem.getInstance().getRefereeByUseName("2");
-        Referee side2 = FootballSystem.getInstance().getRefereeByUseName("3");
-        Game game = null;
-
-        try {
-            game = new Game(date,time,field,team1,team2,main,side1,side2,season,league);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        main.addGame(game);
-        FootballSystem.getInstance().addGameToDB(game);
+        JDBCConnector jdbcConnector = new JDBCConnector();
+        jdbcConnector.connectDBUploadData();
+//        Season season = new Season("2020");
+//        Season season1 = new Season("2021");
+//        FootballSystem.getInstance().addSeasonToDB(season);
+//        FootballSystem.getInstance().addSeasonToDB(season1);
+//        League league = new League("La Liga");
+//        League league1 = new League("liga");
+//        FootballSystem.getInstance().addLeagueToDB(league);
+//        FootballSystem.getInstance().addLeagueToDB(league1);
+//        league.addSeasonToLeague(season);
+//        Field field = new Field("Blomfield","teal aviv",1000);
+//        Field field1 = new Field("Tedi","teal aviv",1000);
+//        FootballSystem.getInstance().addFieldToDB(field);
+//        FootballSystem.getInstance().addFieldToDB(field1);
+//        FootballSystem.getInstance().signIn("4","4","tair","cohen");
+//        TeamOwner tairTO = (TeamOwner) FootballSystem.getInstance().creatingTeamOwner("4","tair","cohen");
+//        FootballSystem.getInstance().signIn("1","1","lala","la");
+//        FootballSystem.getInstance().creatingReferee("1","la","laala", EReferee.MAIN);
+//        FootballSystem.getInstance().signIn("2","2","lala","la");
+//        FootballSystem.getInstance().creatingReferee("2","la","laala", EReferee.ASSISTANT);
+//        FootballSystem.getInstance().signIn("3","3","lala","la");
+//        FootballSystem.getInstance().creatingReferee("3","la","laala", EReferee.ASSISTANT);
+//
+//        Team team1 = new Team(45,"team1",season,field,null,(TeamOwner)FootballSystem.getInstance().getFanByUserName("4"));
+//        Team team2 = new Team(55,"team2",season,field,null,(TeamOwner)FootballSystem.getInstance().getFanByUserName("4"));
+//        team1.addSeasonToTeam(season);
+//        team1.addSeasonToTeam(season1);
+//        team2.addSeasonToTeam(season);
+//        tairTO.addCoach(team1,season,"coach-2020","123","c","c", ETraining.CDiploma,ECoachRole.AssistantCoach);
+//        tairTO.addField(team1,season,"Camp-No1","tel-aviv",12222);
+//        tairTO.addField(team1,season,"Camp-No2","tel-aviv",12222);
+//        tairTO.addTeamManager(team1,season,"TM-2020","123","la","la");
+//        tairTO.addPlayer(team1,season,"player-2020","123","la","la",new Date(17/10/1995),EPlayerRole.GoalKeeper);
+//
+//        try {
+//            FootballSystem.getInstance().addTeamToDB(team1);
+//            FootballSystem.getInstance().addTeamToDB(team2);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        Date date = new Date(2020-1900,4,12);
+//        Time time = new Time(14,40,0);
+//        Referee main = FootballSystem.getInstance().getRefereeByUseName("1");
+//        Referee side1 = FootballSystem.getInstance().getRefereeByUseName("2");
+//        Referee side2 = FootballSystem.getInstance().getRefereeByUseName("3");
+//        Game game = null;
+//
+//        try {
+//            game = new Game(date,time,field,team1,team2,main,side1,side2,season,league);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        main.addGame(game);
+//        FootballSystem.getInstance().addGameToDB(game);
     }
 
     public static void main(String[] args) {
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,6 +1,8 @@
 package Server;
 
+import DAL.JDBCConnector;
 import Server.Strategies.IServerStrategy;
+import org.junit.rules.Timeout;
 
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
@@ -31,16 +33,25 @@ public class Server {
 
     private void runServer() {
         try {
+            int counter = 0;
             ServerSocket serverSocket = new ServerSocket(port);
             serverSocket.setSoTimeout(listeningInterval);
             while (!stop) {
                 try {
+                    counter++;
                     Socket clientSocket = serverSocket.accept(); // blocking call
                     handleClient(clientSocket);
 //                    new Thread(() -> {
 //                        handleClient(clientSocket);
 //                    }).start();
                 } catch (SocketTimeoutException e) {
+
+                }
+                if(counter == 1000){
+                    JDBCConnector jdbcConnector = new JDBCConnector();
+                    jdbcConnector.connectDBSaveData();
+                    jdbcConnector.connectDBUploadData();
+                    counter = 0;
                 }
             }
             serverSocket.close();
